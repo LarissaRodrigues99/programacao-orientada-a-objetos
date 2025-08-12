8ab63e6c19320bf5d5ae07ccf4b18c7ba6b31de1
Larissa Rodrigues <larissarodrigues1599@gmail.com>
2025-08-12 18:52:32 -0300

    Criando luta

---
+++ Relacionamento entre classes/ultraEmojiCombat/src/Luta.java
@@ -0,0 +1,84 @@
+import java.util.Random;
+
+public class Luta {
+    //Atributo
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int ronds;
+    private boolean aprovada;
+    //Metodos publicos
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria() == l2.getCategoria() && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        }else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+    }
+    public void lutar(){
+        if(this.aprovada){
+            System.out.println("### Desafiado ###");
+            this.desafiado.apresentar();
+            System.out.println("### Desafiante ###");
+            this.desafiante.apresentar();
+
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); //0 1 2
+            switch (vencedor){
+                case 0: //Empate
+                    System.out.println("Empatou!");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1: //Desafiado vence
+                    System.out.println("Votória do " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2: //Desafiante vence
+                    System.out.println("Vitória do " + this.desafiante.getNome());
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+                    break;
+            }
+        }else {
+            System.out.println("A luta não pode acontecer");
+        }
+    }
+    //Metodos especiais
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRonds() {
+        return ronds;
+    }
+
+    public void setRonds(int ronds) {
+        this.ronds = ronds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+}
